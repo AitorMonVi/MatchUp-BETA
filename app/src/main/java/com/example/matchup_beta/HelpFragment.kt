9d6c36f68012d561c748e57623437b6586771ab8
andresgonzalez03 <2023_andres.gonzalez@iticbcn.cat>
2024-12-17 19:21:01 +0100

    afegit menu hamburguesa amb toolbar. canviats png per vectors per fer-ho més escalable. canviades activities per fragments, encara continuem migrant-ho.

---
+++ app/src/main/java/com/example/matchup_beta/HelpFragment.kt
@@ -0,0 +1,26 @@
+package com.example.matchup_beta
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HelpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_help, container, false)
+        ViewCompat.setOnApplyWindowInsetsListener(view.findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        return view
+    }
+}

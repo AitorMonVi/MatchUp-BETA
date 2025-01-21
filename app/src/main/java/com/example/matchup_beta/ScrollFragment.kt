2be2b5eda2bd3d473b4b1b384e8826985620b470
andresgonzalez03 <2023_andres.gonzalez@iticbcn.cat>
2025-01-21 16:47:43 +0100

    pasadas todas las actividades a fragments

--- app/src/main/java/com/example/matchup_beta/ScrollFragment.kt
+++ app/src/main/java/com/example/matchup_beta/ScrollFragment.kt
@@ -1,9 +1,7 @@
 package com.example.matchup_beta
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment

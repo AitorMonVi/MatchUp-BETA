5b2383353df873e8873d2f2a10f50ea49344fd4e
AitorMonVi <2023_Aitor.Montejo@iticbcn.cat>
2025-03-17 19:01:50 +0100

    añadidos cambios en el fragment del profile

--- app/src/main/java/com/example/matchup_beta/ProfileFragment.kt
+++ app/src/main/java/com/example/matchup_beta/ProfileFragment.kt
@@ -4,17 +4,66 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import androidx.transition.Visibility
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var nameText : EditText
+    private lateinit var ageText : EditText
+    private lateinit var descriptionText : EditText
+    private lateinit var editProfile : ImageButton
+    private lateinit var makeChanges : Button
+
+    private var click : Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        // declaramos botones, text, etc
+        nameText = view.findViewById(R.id.name)
+        ageText = view.findViewById(R.id.age)
+        descriptionText = view.findViewById(R.id.description)
+        editProfile = view.findViewById(R.id.edit_profile)
+        makeChanges = view.findViewById(R.id.make_changes)
+
+        // hacemos invisible el boton para hacer cambios
+        makeChanges.visibility = View.INVISIBLE
+        nameText.isEnabled = false
+        ageText.isEnabled = false
+        descriptionText.isEnabled = false
+
+        editProfile.setOnClickListener{
+            if (!click) {
+                makeChanges.visibility = View.VISIBLE
+                nameText.isEnabled = true
+                ageText.isEnabled = true
+                descriptionText.isEnabled = true
+                click = true
+            } else {
+                makeChanges.visibility = View.INVISIBLE
+                nameText.isEnabled = false
+                ageText.isEnabled = false
+                descriptionText.isEnabled = false
+                click = false
+            }
+        }
+
+        makeChanges.setOnClickListener{
+            makeChanges.visibility = View.INVISIBLE
+            nameText.isEnabled = false
+            ageText.isEnabled = false
+            descriptionText.isEnabled = false
+            click = false
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(view.findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
